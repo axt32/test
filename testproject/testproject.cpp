9a7b2d01a9d33d525adb5b11aa3e534083918b93
axt32 <axt32@naver.com>
2017-06-05 14:04:07 +0900

    test

---
+++ testproject/testproject.cpp
@@ -0,0 +1,13 @@
+// testproject.cpp : 콘솔 응용 프로그램에 대한 진입점을 정의합니다.
+//
+
+#include "stdafx.h"
+
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+	printf ("This is a test project for studying GitHub.\n");
+
+	return 0;
+}
+
